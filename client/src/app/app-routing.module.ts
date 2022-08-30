09227148df39d01f6c9f02efb5424d0aad9ba54b
Khushboo Goyal <Khushboo.Goyal@microtec.eu>
2022-08-30 10:52:39 +0300

    Bootstrap theme changed, auth guard added, error notification added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componnents/home/home.component';
+import { ListsComponent } from './componnents/lists/lists.component';
+import { MemberDetailComponent } from './componnents/members/member-detail/member-detail.component';
+import { MembersListComponent } from './componnents/members/members-list/members-list.component';
+import { MessagesComponent } from './componnents/messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MembersListComponent, canActivate: [AuthGuard] },
+      { path: 'members/:id', component: MemberDetailComponent },
+      { path: 'lists', component: ListsComponent },
+      { path: 'messages', component: MessagesComponent },
+      { path: '**', component: HomeComponent, pathMatch: 'full' },
+    ]
+  },
+  { path: '**', component: HomeComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,18 +4,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
+import { NavComponent } from './componnents/nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './componnents/home/home.component';
+import { RegisterComponent } from './componnents/register/register.component';
+import { MembersListComponent } from './componnents/members/members-list/members-list.component';
+import { MemberDetailComponent } from './componnents/members/member-detail/member-detail.component';
+import { ListsComponent } from './componnents/lists/lists.component';
+import { ToastrModule } from 'ngx-toastr';
+import { MessagesComponent } from './componnents/messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MembersListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +32,10 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
